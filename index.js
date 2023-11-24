e6c5310140356e8582c72fa3cbe30a9e59340d45
Leonardo Bernardes <lbernardes@santillana.com>
2023-11-23 22:42:25 -0300

    Melhoria nos efeitos e distribuição dos conteúdos

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function generateRandomString(num) {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const characters = 'Leonardo';
     let result1 = ' ';
     const charactersLength = characters.length;
     for (let i = 0; i < num; i++) {
@@ -8,15 +8,15 @@ function generateRandomString(num) {
 
     return result1;
 }
-function generateRandomNumberMatrix(num) {
+function generateRandomNumberMatrix(num, valorMinimo) {
     const characters = '0123456789';
-    let result1 = ' ';
+    let result1 = '';
     const charactersLength = characters.length;
     for (let i = 0; i < num; i++) {
         result1 += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
-
-    return parseFloat(result1);
+    let r = parseFloat(result1);
+    return r > valorMinimo ? r : valorMinimo;
 }
 
 function createSectionMatrix(idSection) {
@@ -25,14 +25,14 @@ function createSectionMatrix(idSection) {
     let heightPage = section.offsetHeight;
 
     let qtdColumns = widthPage / 25;
-    let qtdrows = heightPage / 20;
+    let qtdrows = heightPage / 100;
 
     let countRow = 0;
     while (countRow < qtdrows) {
 
         let newDivRow = document.createElement("div");
         newDivRow.style.width = '100%';
-        newDivRow.style.height = '20px';
+        newDivRow.style.height = "100px";
         newDivRow.style.flexDirection = 'row';
         newDivRow.style.backgroundColor = '#000';
         section.appendChild(newDivRow);
@@ -41,8 +41,7 @@ function createSectionMatrix(idSection) {
         while (countColumn < qtdColumns) {
             let newDivColumn = document.createElement("div");
 
-
-            newDivColumn.style.animation = "blip " + generateRandomNumberMatrix(1) + "s infinite alternate";
+            newDivColumn.style.animation = "makeItRain " + generateRandomNumberMatrix(1, 3) + "s infinite linear";
             newDivColumn.classList = 'divMatrix';
             newDivRow.appendChild(newDivColumn);
 
